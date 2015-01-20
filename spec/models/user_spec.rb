b1f46a8e8b30aefb59fe486b065812a9348f101d
Laura Whalin <laurawhalin@gmail.com>
2015-01-20 15:36:03 -0700

    hound

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) {
               User.create(
-                          fullname: "Daisy",
-                          display_name: "lildaisy",
-                          email: "daisy@example.com",
-                          password: "password",
-                          role: "default"
+                          fullname: 'Daisy',
+                          display_name: 'lildaisy',
+                          email: 'daisy@example.com',
+                          password: 'password',
+                          role: 'default'
                           )
   }
 
@@ -28,11 +28,11 @@ RSpec.describe User, type: :model do
   it 'is not valid without a unique email' do
     user
     user_2 = User.new(
-                      fullname: "Daisy",
-                      display_name: "lildaisy",
-                      email: "daisy@example.com",
-                      password: "password",
-                      role: "default"
+                      fullname: 'Daisy',
+                      display_name: 'lildaisy',
+                      email: 'daisy@example.com',
+                      password: 'password',
+                      role: 'default'
                       )
     expect(user_2).to_not be_valid
   end
@@ -49,22 +49,22 @@ RSpec.describe User, type: :model do
 
   it 'is not valid with a display name under 2 characters' do
     user_2 = User.new(
-                      fullname: "Daisy",
-                      display_name: "l",
-                      email: "daisy@example.com",
-                      password: "password",
-                      role: "default"
+                      fullname: 'Daisy',
+                      display_name: 'l',
+                      email: 'daisy@example.com',
+                      password: 'password',
+                      role: 'default'
                       )
     expect(user_2).to_not be_valid
   end
 
   it 'is not valid with a display name over 32 characters' do
     user_2 = User.new(
-                      fullname: "Daisy",
-                      display_name: "llskdjflawierlansvllaiwjekajdsfljkwelkjla",
-                      email: "daisy@example.com",
-                      password: "password",
-                      role: "default"
+                      fullname: 'Daisy',
+                      display_name: 'llskdjflawierlansvllaiwjekajdsfljkwelkjla',
+                      email: 'daisy@example.com',
+                      password: 'password',
+                      role: 'default'
                       )
     expect(user_2).to_not be_valid
   end
@@ -76,7 +76,7 @@ RSpec.describe User, type: :model do
 
   it 'can have many orders' do
     skip
-    user.orders.create(status: "pending", total: "2434")
+    user.orders.create(status: 'pending', total: '2434')
     expect(user.orders.map(&:user_id)).to eq([user.id])
   end
 end
