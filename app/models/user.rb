564d6667c299aecf80505273fbc3ee9f9f910198
Laura Whalin <laurawhalin@gmail.com>
2015-01-20 15:19:16 -0700

    created user model, and added tests to validate the parameters.

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates :fullname, :password, :role, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :display_name, length: { in: 2..32 }, allow_nil: true
+
+end
