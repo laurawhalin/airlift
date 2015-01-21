510ef84ee6a4c3a746a4237b760a7f50cdc9d6f5
Laura Whalin <laurawhalin@gmail.com>
2015-01-20 21:57:00 -0700

    hound

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  let(:category) { Category.new(name: "burritos", description: "all the burritos") }
+  let(:category) {
+                  Category.new(
+                               name: "burritos",
+                               description: "all the burritos"
+                               )
+  }
 
   it "is valid" do
     expect(category).to be_valid
@@ -18,7 +23,12 @@ RSpec.describe Category, type: :model do
   end
 
   xit "can have many items" do
-    category.items.create(title: "Green Chili Burrito", description: "Amazingly spicey burrito", price:655, image: "image/path")
+    category.items.create(
+                          title: "Green Chili Burrito",
+                          description: "Amazingly spicey burrito",
+                          price: 655,
+                          image: "image/path"
+                          )
     expect(category.items.map(&:category_id)).to eq([category.id])
   end
 end
