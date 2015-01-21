d7f67b040f514693d2f18fb83975d3f8193f23ff
Brandon Mrsny <bmrsny@gmail.com>
2015-01-21 12:03:35 -0700

    finished the category seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def initialize
+    build_items
+    build_category
+  end
+
+  def build_items
+    item1 = Item.create(title: "Gut Buster", description: "If its in the store, it's in this burrito!", price: 1000, image:"burrito_360.png")
+    item2 = Item.create(title: "", description: "If its in the store, it's in this burrito!", price: 1000, image:"burrito_360.png")
+  end
+
+  def build_category
+    category1 = Category.create(name: "Vegetarian", description: "Meatless options.")
+    category2 = Category.create(name: "Spicy", description: "Hot! Hot! Hot!")
+    category3 = Category.create(name: "Specialty", description: "Chef's favorites.")
+    category4 = Category.create(name: "Breakfast", description: "Start your day off right.")
+    category5 = Category.create(name: "Sides", description: "&#192; la carte options.")
+  end
+end
+
+Seed.new
