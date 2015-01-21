d6067fe5054fe53a37f2fde22ef2372e6a718cd7
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-01-21 12:50:23 -0700

    added 5 more items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@ class Seed
 
   def build_items
     item1 = Item.create(title: "Gut Buster", description: "If its in the store, it's in this burrito!", price: 1000, image:"burrito_360.png")
-    item2 = Item.create(title: "", description: "", price: , image:"burrito_360.png")
+    item11 = Item.create(title: "Halo", description: "All fresh vegetables which gives you the moring shine", price: 500, image:"burrito_360.png")
+    item12 = Item.create(title: "Vegi delight", description: "Eat all your vegies, tomatoes, red onions, green pepers, mushrooms and more)", price: 700, image:"burrito_360.png")
+    item13 = Item.create(title: "Not too Sweet", description: "Just enough to satisfy for sweet quench, all sweet things", price: 500, image:"burrito_360.png")
+    item14 = Item.create(title: "Classic", description: "Our same classic burrito with it's authentic flavors", price: 800, image:"burrito_360.png")
+    item15 = Item.create(title: "Hot n Spicy", description: "All the hot and spicy things you can handle", price: 1000, image:"burrito_360.png")
   end
 
   def build_category
