485f8c636e3bae62e3c390671246ffe85a6728dd
Brandon Mrsny <bmrsny@gmail.com>
2015-01-20 18:06:44 -0700

    working on broken password

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(id: params[:session][:id])
+    if @user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = "Invalid login"
+      render :new
+    end
+  end
+end
---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+require 'capybara/rspec'
+
+feature "User login" do
+  background do
+    User.create(:email => "jimbobfrank@aol.com", :password => 'foobar1234' )
+  end
+
+  scenario "User can sign in with correct credentials" do
+    visit "/login"
+      within("#session") do
+      fill_in "session[email]", :with => "jimbobfrank@aol.com"
+      fill_in "session[password]", :with => "foobar1234"
+      fill_in "session[password_confirmation]", :with => "foobar1234"
+    end
+    click_button "Login"
+
+    expect(page).to have_content("You have successfully logged in")
+  end
+end
