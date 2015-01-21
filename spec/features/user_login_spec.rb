da61c481436370eb0ae9c731880d1fd51697fdd6
Brandon Mrsny <bmrsny@gmail.com>
2015-01-20 21:32:07 -0700

    mad at the hound

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,23 +2,23 @@ require "rails_helper"
 
 feature "User login" do
   background do
-    @user = User.create(
+  @user = User.create(
     fullname: "jimbobfrank",
     email: "jimbobfrank@aol.com",
     role:  0,
     password: 'foobar1234',
     password_confirmation: 'foobar1234',
     display_name: "frankyboy"
-    )
+  )
   end
 
   scenario "User can sign in with correct credentials" do
     visit "/"
       within("#session") do
-        fill_in "session[email]", :with => "jimbobfrank@aol.com"
-        fill_in "session[password]", :with => "foobar1234"
-        fill_in "session[password_confirmation]", :with => "foobar1234"
-    end
+        fill_in "session[email]", with: "jimbobfrank@aol.com"
+        fill_in "session[password]", with: "foobar1234"
+        fill_in "session[password_confirmation]", with: "foobar1234"
+      end
     click_link_or_button "Login"
     within("#success") do
       expect(page).to have_content("You have successfully logged in")
