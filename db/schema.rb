64058a23a5ee88178d36427355545385a11a3a5b
Brandon Mrsny <bmrsny@gmail.com>
2015-01-20 21:18:30 -0700

    migrate db after merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150121011141) do
-
+ActiveRecord::Schema.define(version: 20150121031003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "items_categories", force: :cascade do |t|
-    t.integer  "item_id"
-    t.integer  "category_id"
-  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.integer  "price"
     t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "items_categories", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
