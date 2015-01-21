62295a9661962e2c1f71ba11064371ff0dd7c4f8
Brandon Mrsny <bmrsny@gmail.com>
2015-01-20 21:39:27 -0700

    getting hounded

--- Gemfile
+++ Gemfile
@@ -25,10 +25,10 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
- gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
- gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -37,7 +37,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'better_errors'
-gem 'binding_of_caller'
+  gem 'binding_of_caller'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -3,22 +3,22 @@ require "rails_helper"
 feature "User login" do
   background do
   @user = User.create(
-    fullname: "jimbobfrank",
-    email: "jimbobfrank@aol.com",
-    role:  0,
-    password: 'foobar1234',
-    password_confirmation: 'foobar1234',
-    display_name: "frankyboy"
+  fullname: "jimbobfrank",
+  email: "jimbobfrank@aol.com",
+  role:  0,
+  password: "foobar1234",
+  password_confirmation: "foobar1234",
+  display_name: "frankyboy"
   )
   end
 
   scenario "User can sign in with correct credentials" do
     visit "/"
-      within("#session") do
-        fill_in "session[email]", with: "jimbobfrank@aol.com"
-        fill_in "session[password]", with: "foobar1234"
-        fill_in "session[password_confirmation]", with: "foobar1234"
-      end
+    within("#session") do
+      fill_in "session[email]", with: "jimbobfrank@aol.com"
+      fill_in "session[password]", with: "foobar1234"
+      fill_in "session[password_confirmation]", with: "foobar1234"
+    end
     click_link_or_button "Login"
     within("#success") do
       expect(page).to have_content("You have successfully logged in")
