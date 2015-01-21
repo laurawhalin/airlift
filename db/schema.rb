84130ef0bc36448575e6a95146156778b548e62f
Laura Whalin <laurawhalin@gmail.com>
2015-01-20 20:32:57 -0700

    added items category model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120184912) do
+ActiveRecord::Schema.define(version: 20150121031003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items_categories", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "fullname"
