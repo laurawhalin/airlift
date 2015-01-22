779875466d9e1a1ea21b0b28605bf3a5c7aa7393
Laura Whalin <laurawhalin@gmail.com>
2015-01-22 12:54:36 -0700

    add to cart button placed on items index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users, :items, :cart_items
   root "welcome#index"
-  resources :items
 end
