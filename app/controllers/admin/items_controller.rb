d0c1ef479d386e48f32add51c6346e384364b788
Brandon Mrsny <bmrsny@gmail.com>
2015-01-25 23:11:34 -0700

    admin can edit items initiallly working, need images and failure messages

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,6 +12,16 @@ module Admin
       Item.create(item_params)
       redirect_to admin_items_path
     end
+
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+      redirect_to admin_items_path
+    end
   end
   # private
   # why you no work here??? only works in Admin Controller
---
+++ spec/features/admin_can_edit_items_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+feature "Admin User Items" do
+  background do
+    @admin_user = User.create(fullname: "frank", email: "frank@aol.com", role: "admin",
+    password: "foobar1234", password_confirmation: "foobar1234", display_name: "franky")
+  end
+
+  scenario "Admin User can edit current items and see them updated on index page" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user)
+    .and_return(@admin_user)
+    item = Item.create(title:"yum", description: "you bet", price: 12300)
+
+    visit admin_items_path
+    click_link_or_button "Edit Item"
+    expect(current_path).to eq(edit_admin_item_path(item))
+
+    fill_in "item[title]", with: "new"
+    fill_in "item[description]", with: "this is it"
+    fill_in "item[price]", with: "2200"
+    click_link_or_button 'Save'
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("new")
+    expect(page).to have_content("this is it")
+    expect(page).to have_content("$22.00")
+  end
+end
