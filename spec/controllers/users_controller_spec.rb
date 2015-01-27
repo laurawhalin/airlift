8a1d7701d7182968667f798a002cd7ce7cf28194
Brandon Mrsny <bmrsny@gmail.com>
2015-01-27 16:45:13 -0700

    fix dem hounds son

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe UsersController, :type => :controller do
-  it 'can create user' do
+describe UsersController, type: :controller do
+  it "can create user" do
     expect do
-      post :create, user: {fullname: 'Jo Jo', email: 'jojo@example.com', displayname: 'jojo', password: 'jojojojo', password_confirmation: 'jojojojo' }
+      post :create, user: {fullname: "Jo Jo", email: "jojo@example.com", displayname: "jojo", password: "jojojojo", password_confirmation: "jojojojo" }
     end.to change(User, :count).by(1)
     response.should be_redirect
   end
 
-  it 'cannot create admin' do
+  it "cannot create admin" do
     expect do
-      post :create, user: {fullname: 'Jo Jo', email: 'jojo@example.com', displayname: 'jojo', password: 'jojojojo', password_confirmation: 'jojojojo', role: 'admin' }
+      post :create, user: {fullname: "Jo Jo", email: "jojo@example.com", displayname: "jojo", password: "jojojojo", password_confirmation: "jojojojo", role: "admin" }
     end.to change(User, :count).by(1)
     # response.should_not be_
     expect(User.last.role).to eq("default")
