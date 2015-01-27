cf06df2a80bccd13aa321ed1d27e0c7baf0d7146
Brandon Mrsny <bmrsny@gmail.com>
2015-01-26 17:03:45 -0700

    fixing the hounds

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "burrito_360.png"
+  validates :price, presence: true, numericality: { :greater_than_or_equal_to => 0 }
+  has_attached_file :image, :styles => { medium: "300x300>",
+                                        thumb: "100x100>" },
+                                        default_url: "burrito_360.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :items_categories
   has_many :categories, through: :items_categories
