9d722ad49707afe1c661a239ce5ef19cde3e89a0
Brandon Mrsny <bmrsny@gmail.com>
2015-01-28 16:57:02 -0700

    finished admin can order status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -18,6 +18,12 @@ module Admin
       if params[:commit] == "cancel"
         @order.update(status: "cancelled")
         redirect_to admin_orders_path
+      elsif params[:commit] == "mark as paid"
+        @order.update(status: "paid")
+        redirect_to admin_orders_path
+      elsif params[:commit] == "mark as completed"
+        @order.update(status: "completed")
+        redirect_to admin_orders_path
       end
     end
   end
