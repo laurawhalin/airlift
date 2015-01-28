7cd43fc6e600601eb29a57b9d1bc1004db800784
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-01-28 10:47:22 -0700

    hound

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,11 @@
 module Admin
   class OrdersController < ApplicationController
     def index
-      @orders = {ordered: Order.ordered, paid: Order.paid, completed: Order.completed, cancelled: Order.cancelled}
+      @orders = { ordered: Order.ordered,
+                  paid: Order.paid,
+                  completed: Order.completed,
+                  cancelled: Order.cancelled
+                }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-
   namespace :admin do
     resources :categories
     resources :dashboard
