92c588908486c6e4a61ec5fdffbf31f5c9676f7b
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-01-28 13:16:20 -0700

    displayed name and email with user order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,5 +7,10 @@ module Admin
                   cancelled: Order.cancelled
                 }
     end
+
+    def show
+      @order = Order.find(params[:id])
+      @user = User.find(@order.user_id)
+    end
   end
 end
