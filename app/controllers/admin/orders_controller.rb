1e2fc24ad8647462deff16ebadd7cf28039a31d0
Brandon Mrsny <bmrsny@gmail.com>
2015-01-29 10:52:32 -0700

    took out the commented code

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -17,17 +17,6 @@ module Admin
 
     def update
       change_status
-      # @order = Order.find(params[:id])
-      # if params[:commit] == "cancel"
-      #   @order.update(status: "cancelled")
-      #   redirect_to admin_orders_path
-      # elsif params[:commit] == "mark as paid"
-      #   @order.update(status: "paid")
-      #   redirect_to admin_orders_path
-      # elsif params[:commit] == "mark as completed"
-      #   @order.update(status: "completed")
-      #   redirect_to admin_orders_path
-      # end
     end
   end
 end
