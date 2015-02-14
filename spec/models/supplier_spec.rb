f0b49fa083c19486e3e642c7324ec5d4a13854e6
Rich Shea <richcshea@gmail.com>
2015-02-14 13:04:28 -0700

    add supplier can have many listings

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, :type => :model do
-  
-  it "has an item" do
-    supplier = Supplier.create(supplier_attributes)
-    supplier = supplier.item.create(item_attributes)
-    
-  
+
+  describe "Supplier" do
+     it "has an item" do
+        supplier = Supplier.create(supplier_attributes)
+        item = Item.create(item_attributes)
+        supplier.listings.create(item_id: item.id, quantity: 5)
+
+        expect(supplier.listings.count).to eq(1)
+        expect(supplier.listings.first.item_id).to eq(1)
+     end
+  end
 end
