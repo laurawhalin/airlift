33f091a5900189d8ba666a5a51e9d9a9de7773e3
Rich Shea <richcshea@gmail.com>
2015-02-14 12:19:03 -0700

    begin spec for items belong to supplier

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Supplier, :type => :model do
   
+  it "has an item" do
+    supplier = Supplier.create(supplier_attributes)
+    supplier = supplier.item.create(item_attributes)
+    
+  
 end
