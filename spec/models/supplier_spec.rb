4cef566b5a4c60f94e9a61b937b0a18ae483720e
Brandon Mrsny <bmrsny@gmail.com>
2015-02-15 15:14:14 -0700

    fixed broken test

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Supplier, :type => :model do
         supplier.listings.create(item_id: item.id, quantity: 5)
 
         expect(supplier.listings.count).to eq(1)
-        expect(supplier.listings.first.item_id).to eq(1)
+        expect(supplier.listings.first.item_id).to eq(38)
      end
   end
 end
