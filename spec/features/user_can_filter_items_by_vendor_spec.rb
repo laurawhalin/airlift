e7244a31df34f0b7dcf976a82c5e8c3042b8243d
Brandon Mrsny <bmrsny@gmail.com>
2015-02-16 19:00:15 -0700

    tests are still faling ftw

--- spec/features/user_can_filter_items_by_vendor_spec.rb
+++ spec/features/user_can_filter_items_by_vendor_spec.rb
@@ -4,6 +4,7 @@ feature "User Supplier Items" do
 	
 	scenario "User visits items index page" do
 		user = User.create(user_attributes)
+		supplier = Supplier.create(supplier_attributes)
 		item = Item.create(item_attributes)
 		visit items_path 
 		expect(page).to have_content("water purifier")	
@@ -11,15 +12,14 @@ feature "User Supplier Items" do
 
 	scenario "User filters items by a vendor" do
 		supplier = Supplier.create(supplier_attributes)
-		supplier.items.create(item_attributes)
-		
+		supplier2 = Supplier.create(supplier_attributes(name: "nado", slug: "nado"))
+		item = supplier.items.create(item_attributes)
+		item2 = supplier2.items.create(item_attributes(title: "beans", supplier_id: 2))	
 		visit items_path
-		within(".supplier_list") do
-			find_link("Fireproof").visible?
-		end
 
-		click_link_or_button("Fireproof")
-		expect(page).to	have_content(supplier.items.first.title)
+		find(:css, "#current_suppliers_fireproof[type='checkbox']").set(true)
+		expect(page).to	have_content(item.title)
+		#expect(page).to_not have_content(item2.title)
 	end
 
 	scenario "User can view item's aggregate data by clicking link" do
