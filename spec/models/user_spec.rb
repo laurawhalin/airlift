455e085c16ca2a4d692b9ebba2b09a43bff79811
Alex Robinson <alexandra.scott335@gmail.com>
2015-02-18 12:51:51 -0700

    fixed failing test and merged in with jquery_vendor branch

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,6 +89,6 @@ RSpec.describe User, type: :model do
   end
 
   it "can find it admin_supplier id" do
-    expect(user.find_supplier_admin(user.id).id).to eq(user.id)
+    expect(user.find_supplier_admin(user.id).id).to eq(supplier_admin.id)
   end
 end
