e325c57b2e82b808262d794f7a3349b3e373507b
Rich Shea <richcshea@gmail.com>
2015-02-20 17:05:51 -0700

    missing id for show page

--- app/controllers/suppliers/orders_controller.rb
+++ app/controllers/suppliers/orders_controller.rb
@@ -1,2 +1,13 @@
 class Suppliers::OrdersController < SuppliersController
+  def index
+    @orders = { ordered: Order.ordered,
+                paid: Order.paid,
+                completed: Order.completed,
+                cancelled: Order.cancelled
+              }
+    @all_orders = Order.all
+  end
+
+  def show
+  end
 end
