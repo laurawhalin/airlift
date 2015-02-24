489cd1dcc77415aba0a0571a865297c867c45674
Alex Robinson <alexandra.scott335@gmail.com>
2015-02-23 17:42:45 -0700

    added authorization for supplier

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    if user.admin? || user.supplier?
+    if user.admin?
       can :manage, :all
-#		elsif user.supplier?
-#			can [:read, :update], Supplier, id: user.supplier_admin.supplier.id
-#			can :manage, [Item, Category, Order], id: user.supplier_admin.supplier.id
-#
+		elsif user.supplier?
+			can [:read, :update], Supplier, slug: user.supplier_admin.supplier.slug
+			can [:manage], [Item, Order, Category], supplier_id: user.supplier_admin.supplier.id
+      cannot [:manage], Cart, id: user.id
     else
       can :read, User, id: user.id
       can :read, Order#, user_id: user.id  not sure if I should replace this with something else that identifies the user?
