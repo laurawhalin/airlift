2b355360e0a2a585375271043cc0d35073cdf379
Brandon Mrsny <bmrsny@gmail.com>
2015-02-24 11:50:50 -0700

    fixed bug issues in updating SupplierAdmin roles

--- app/controllers/suppliers/users_controller.rb
+++ app/controllers/suppliers/users_controller.rb
@@ -2,11 +2,12 @@ class Suppliers::UsersController < SuppliersController
 
 	def update
 		@user =  User.find(params[:id])
+		role = params[:user][:role]
 		@supplier_admin = SupplierAdmin.find_by(user_id: @user.id)
-		if @user.update(supplier_admin_params) && @user.role == 1
+		if @user.update(supplier_admin_params) && role == "supplier"
 			flash[:success] = "User successfully updated"
 			redirect_to supplier_path(@user.supplier_slug(@user.id))
-		elsif @user.role == 1
+		elsif role == "default"
 			@supplier_admin.delete
 			redirect_to :back
 		else
